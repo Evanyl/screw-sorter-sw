ee6f2c1d8e76e29af1d2d0174c9bb91a7394b58e
kxing28 <kenneth.xing@alumni.ubc.ca>
2024-02-12 14:16:31 -0800

    update platformio ver

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -2,6 +2,7 @@
 #include "app/scheduler.h"
 #include "app/cli.h"
 #include "app/depositor.h"
+#include "app/imaging.h"
 #include "app/motor_runner.h"
 
 #include "dev/servo.h"
@@ -11,6 +12,7 @@ void setup()
     // initialization of applications
     scheduler_init();
     depositor_init();
+    imaging_init();
     cli_init();
     motor_runner_init(); // motors must be initialized first
 }
@@ -25,6 +27,7 @@ void loop()
 
     // 10ms tasks
     // depositor_run10ms();
+    // imaging_run10ms();
 
     // 100ms tasks
     cli_run100ms();
