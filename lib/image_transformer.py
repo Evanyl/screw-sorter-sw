26ccdf26870e98241af61eb2130d6d336ef86918
Evan Liu <liuyievan@gmail.com>
2024-03-07 20:51:48 -0800

    Update thresholding for real

--- lib/image_transformer.py
+++ lib/image_transformer.py
@@ -350,7 +350,7 @@ def transform_side_image(read_fpath, crop_w=800/2, crop_h=800/2, top_plane_y=160
     img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
 
     blur = cv2.blur(img,(13,13), 0)
-    _, img = cv2.threshold(blur, 130, 255, cv2.THRESH_BINARY_INV)
+    _, img = cv2.threshold(blur, 30, 255, cv2.THRESH_BINARY_INV)
 
     contours,_ = cv2.findContours(img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
     main_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
