31598dcc7f9694e777682789f35dee62d7898cb6
kxing28 <kenneth.xing@alumni.ubc.ca>
2024-03-16 11:16:12 -0700

    Incomplete patching for HardwareSerial buffer size
    following
    https://docs.platformio.org/en/latest/scripting/examples/override_package_files.html

---
+++ fw/scripts/apply_patches.py
@@ -0,0 +1,24 @@
+from os.path import join, isfile
+import patch
+
+Import("env")
+
+FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduinoststm32")
+patchflag_path = join(FRAMEWORK_DIR, ".patching-done")
+
+# patch file only if we didn't do it before
+if not isfile(join(FRAMEWORK_DIR, ".patching-done")):
+    original_file = join(FRAMEWORK_DIR, "cores", "arduino", "HardwareSerial.h")
+    patched_file = join("patches", "1-HardwareSerial-increase-buffer-size.patch")
+
+    assert isfile(original_file) and isfile(patched_file)
+
+    env.Execute("patch.py %s %s" % (original_file, patched_file))
+    # env.Execute("touch " + patchflag_path)
+
+
+    def _touch(path):
+        with open(path, "w") as fp:
+            fp.write("")
+
+    env.Execute(lambda *args, **kwargs: _touch(patchflag_path))
