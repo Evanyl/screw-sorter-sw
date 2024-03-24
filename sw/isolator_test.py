500aa3b9fa938f2f0cefb9874110e35245dd4a39
Rudransh Kumar <rudransh@gmail.com>
2024-03-23 18:54:03 -0700

    update demo test script

--- sw/isolator_test.py
+++ sw/isolator_test.py
@@ -2,8 +2,23 @@ import cv2
 
 from isolator import Isolator, IsolatorDirective, IsolatorMission, IsolatorWorldView
 
-x = Isolator()
-x.spin(IsolatorMission.ISOLATE, IsolatorWorldView(True, True, True))
-# x.show()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+isolator = Isolator()
+
+while True:
+    # check from firmware if belts are still moving and depositor homed
+    belt1_moving = False
+    belt2_moving = False
+    depositor_homed = True
+
+    # Give isolator it's world view, i.e., external state of the world
+    world = IsolatorWorldView(belt1_moving, belt2_moving, depositor_homed)
+
+    # Give isolator it's mission, i.e., actively isolating or just idling
+    mission = (
+        IsolatorMission.ISOLATE
+    )  # (or IsolatorMission.IDLE, if you want to suppress isolation rn)
+
+    directive = isolator.spin(mission, world)
+
+    print(directive.b1steps)
+    print(directive.b2steps)
