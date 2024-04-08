47b31b383812bc2e1fc6f7795120071a795174a4
Rudransh Kumar <rudransh@gmail.com>
2024-04-08 15:36:35 -0700

    add benchmarking ability

--- sw/isolator_test.py
+++ sw/isolator_test.py
@@ -1,9 +1,11 @@
 import cv2
+import numpy as np
+import time
 
 from isolator import Isolator, IsolatorDirective, IsolatorMission, IsolatorWorldView
 
 isolator = Isolator()
-
+runtime = list()
 while True:
 
     belt1_moving = False
@@ -11,10 +13,17 @@ while True:
     depositor_homed = True
     world = IsolatorWorldView(belt1_moving, belt2_moving, depositor_homed)
     mission = IsolatorMission.ISOLATE
+    t2 = time.perf_counter()
     directive = isolator.spin(mission, world)
+    t1 = time.perf_counter()
     isolator.show()
+    runtime.append(t2 - t1)
     k = cv2.waitKey(0)
     if k == ord("q"):
         break
 
+runtime = np.array(runtime)
+print(f"N: {len(runtime)}")
+print(f"Average Time: {np.mean(runtime)}")
+print(f"Standard Deviation: {np.std(runtime)}")
 cv2.destroyAllWindows()
